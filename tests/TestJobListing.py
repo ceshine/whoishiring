d6b32ad57aba26d85eac0ef24a7a2dc41dbe0562
joshz <jzegan@gmail.com>
2013-09-12 04:38:26 +0200

    couple tests for job listing

---
+++ tests/TestJobListing.py
@@ -0,0 +1,46 @@
+import nose.tools as nt
+from lib import whlisting, utils
+from mock import patch
+from spec import Spec
+import mocks
+from datetime import date
+from random import randint
+from itertools import izip
+from lib.joblisting import JobListing
+from collections import namedtuple
+from datetime import date
+
+
+Item = namedtuple('HNListingItem', ['title', 'permanent', 'url', 'date'], verbose=False)
+
+
+class TestWHListing(Spec):
+    def setup(self):
+        self.patcher_ep = patch.object(utils,
+                                       'get_raw_page',
+                                       autospec=True,
+                                       return_value=mocks.get_page('%s/tests/data/dec2012.html'))
+        self.patcher_ep.start()
+        self.d = date(2013, 3, 1)
+        self.item = Item('test title', True, 'http://fakeurl.com/stuff', self.d)
+
+    def teardown(self):
+        self.patcher_ep.stop()
+
+    def test_listing_has_nonzero_length(self):
+        j = JobListing(self.item)
+        nt.eq_(len(j), 103, "Incorrect object length")
+        print j
+
+    def test_comment_from_correct_date_and_indexing(self):
+        j = JobListing(self.item)
+        nt.eq_(j[10]['date'], self.d, "Incorrect date")
+
+    def test_prints_human_readable_str(self):
+        j = JobListing(self.item)
+        nt.ok_('103' in j.__str__(), "Incorrect or unreadable str")
+
+    def test_iterates_over_comments(self):
+        j = JobListing(self.item)
+        for comment in j:
+            nt.ok_('parent_thread' in comment, 'Iterates over comments in JobListing')
