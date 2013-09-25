db6543ab5f8be45c2b1caf5198884bfcc18ca218
joshz <jzegan@gmail.com>
2013-09-25 09:41:51 +0200

    add setup
    tests require nose with spec plugin

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-__author__ = 'jz'
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+print find_packages(exclude=['tests*'])
+setup(name='whoishiring',
+      version='0.1.0',
+      author='Jan Zegan',
+      description='Get job posts from hn',
+      author_email='jzegan@gmail.com',
+      install_requires=['pip', 'lxml', 'pyquery', 'python-dateutil'],
+      packages=find_packages(exclude=["tests*"])
+      )
